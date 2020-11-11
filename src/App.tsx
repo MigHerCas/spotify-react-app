3396a4db37b4f0f6636bf3dd5613b0b609867cb0
Miguel Hernanz <migherdcas@gmail.com>
2020-11-11 19:02:24 +0100

    feat(OAuth): Fetch access token from auth header.

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 function App(): JSX.Element {
+  const [accessToken, setAccessToken] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const getAccessToken = async (): Promise<void> => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const response = await fetch('https://accounts.spotify.com/api/token', {
+          method: 'post',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            Authorization:
+              'Basic M2YwODRlYjk4Njc1NDdjNDk3MTExN2E4NjY0ZTI3N2Q6Y2NhOGQ0NTc4YTZmNGE0YmJmMmExN2MzMmJjYTNmNzk=',
+          },
+          body: 'grant_type=client_credentials',
+        });
+        const dataJson = await response;
+        const accessTokenResponse = await dataJson.json();
+        setAccessToken(accessTokenResponse);
+      } catch (error) {
+        setIsError(error);
+      }
+
+      setIsLoading(false);
+    };
+
+    getAccessToken();
+  }, []);
+
+  useEffect(() => {
+    console.log(accessToken);
+  }, [accessToken]);
   return (
     <div className="App">
-      <p>I`m joining Mendix soon! (hopefully)</p>
+      <h1>App</h1>
+      {isError && <span>{isError}</span>}
+      {isLoading && <span>{isLoading}</span>}
     </div>
   );
 }
