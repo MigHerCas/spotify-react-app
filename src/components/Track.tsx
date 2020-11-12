88f45831d6bbefe52cfba7abb620ab1f659fb2d5
Miguel Hernanz <migherdcas@gmail.com>
2020-11-12 17:50:33 +0100

    fix(React): Track log disabled temporarily.

--- src/components/Track.tsx
+++ src/components/Track.tsx
@@ -5,23 +5,10 @@ interface Props {
   trackItem: TrackItem;
 }
 
-function Track({ trackItem }: Props): JSX.Element {
-  const { id, name, href, artists } = trackItem;
+export default function Track({ trackItem }: Props): JSX.Element {
   return (
     <div>
-      <div className="track">
-        <p>Track Id: {id}</p>
-        <p>Track name: {name}</p>
-        <p>Track href: {href}</p>
-        <div>
-          {artists &&
-            artists.map((artist) => {
-              return <p key={artist.id}>Artist name: {artist.name}</p>;
-            })}
-        </div>
-      </div>
+      <div className="track" id={trackItem.id} />
     </div>
   );
 }
-
-export default Track;
