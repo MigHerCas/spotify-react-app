8a789cec37ec8d10495e7b2d339bb481b989d2ce
Miguel Hernanz <migherdcas@gmail.com>
2020-11-13 17:51:55 +0100

    feata(Markup): HTML react component -> works better for me.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HTML from './components/HTML';
 import Playlist from './components/Playlist';
 import PlaylistGrid from './components/PlaylistGrid';
 import DEFAULT_PLAYLISTS_IDS from './constants/constants';
@@ -10,6 +11,7 @@ const {
 } = process.env;
 
 export default function App(): JSX.Element {
+  const [markupBuilding] = useState(true);
   const { accessToken } = useToken(
     REACT_APP_SPOTIFY_ACCOUNT_TOKEN_API_URL,
     REACT_APP_SPOTIFY_AUTH_HEADER
@@ -17,19 +19,22 @@ export default function App(): JSX.Element {
 
   return (
     <div className="App">
-      <h1>Spotify React App</h1>
-      <PlaylistGrid>
-        {accessToken &&
-          DEFAULT_PLAYLISTS_IDS.map((playlistId) => {
-            return (
-              <Playlist
-                key={playlistId}
-                playlistId={playlistId}
-                accessToken={accessToken}
-              />
-            );
-          })}
-      </PlaylistGrid>
+      {markupBuilding ? (
+        <HTML />
+      ) : (
+        <PlaylistGrid>
+          {accessToken &&
+            DEFAULT_PLAYLISTS_IDS.map((playlistId) => {
+              return (
+                <Playlist
+                  key={playlistId}
+                  playlistId={playlistId}
+                  accessToken={accessToken}
+                />
+              );
+            })}
+        </PlaylistGrid>
+      )}
     </div>
   );
 }
---
+++ src/components/HTML.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HTML(): JSX.Element {
+  return (
+    <div className="Container">
+      <p>Ma bois</p>
+    </div>
+  );
+}
