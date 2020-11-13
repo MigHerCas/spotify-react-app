e242d60cda0bb9c948e11d5f5e71a06f3ed5efe8
Miguel Hernanz <migherdcas@gmail.com>
2020-11-13 18:24:17 +0100

    feat(Markup): Header part on light theme.

--- src/components/HTML.tsx
+++ src/components/HTML.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import spotifyLogo from '../assets/images/spotiyLogo.png';
 
 export default function HTML(): JSX.Element {
   return (
-    <div className="Container">
-      <p>Ma bois</p>
-    </div>
+    <>
+      <header>
+        <img src={spotifyLogo} alt="Spotify Logo" />
+        <div className="theme-switcher-wrapper">
+          <span>Theme</span>
+          <div className="theme-switcher shadow--dark radius--big" />
+        </div>
+      </header>
+    </>
   );
 }
