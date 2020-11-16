c795dc1e1191c4999e6f59e4a3dee9c026ead3cf
Miguel Hernanz <migherdcas@gmail.com>
2020-11-16 14:02:05 +0100

    feat(React): Handling error in App.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { TrackItem } from './api/track';
 import Grid from './components/layout/Grid';
 import Header from './components/layout/Header';
 import PlaylistsPanel from './components/PlaylistsPanel';
+import ErrorModal from './components/shared/ErrorModal';
 import TracksPanel from './components/TracksPanel';
 import useToken from './hooks/useToken';
 
@@ -12,7 +13,7 @@ const {
 } = process.env;
 
 export default function App(): JSX.Element {
-  const { accessToken } = useToken(
+  const { accessToken, accessError } = useToken(
     REACT_APP_SPOTIFY_ACCOUNT_TOKEN_API_URL,
     REACT_APP_SPOTIFY_AUTH_HEADER
   );
@@ -26,6 +27,11 @@ export default function App(): JSX.Element {
       setTimeout(() => firstTrackRef?.current?.focus(), 300);
     }
   }, [firstTrackRef]);
+
+  if (accessError) {
+    return <ErrorModal error={accessError} />;
+  }
+
   return (
     <div className="App">
       <Header />
